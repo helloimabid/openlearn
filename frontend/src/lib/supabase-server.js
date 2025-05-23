6342c709ec347c3a3d7ef769faf869587cceed5a
Sadman Abid <107977336+helloimabid@users.noreply.github.com>
2025-05-23 12:59:35 +0600

    Update supabase-server.js

--- frontend/src/lib/supabase-server.js
+++ frontend/src/lib/supabase-server.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
 
 export const createServerSupabase = () => {
-  return createClient(import.meta.env.VITE_PUBLIC_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+  return createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
 }
